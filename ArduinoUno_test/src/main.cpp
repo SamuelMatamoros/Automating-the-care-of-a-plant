0c7b477f15250a8e0ab04b1f646ed614a90ec17f
Samuel Matamoros <samuelmatamorosalonso@gmail.com>
2024-01-22 23:43:04 +0100

    Presentación modificada, añadir renders para terminar.

--- ArduinoUno_test/src/main.cpp
+++ ArduinoUno_test/src/main.cpp
@@ -4,37 +4,37 @@
 #include <DHT_U.h>
 #include <Stepper.h>
 
-// DHT_Unified dht(12, DHT11);
+DHT_Unified dht(12, DHT11);
 
-// uint32_t delayMS;
+uint32_t delayMS;
 
-// void setup() {
-//   Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
 
-//   dht.begin();
-//   sensor_t sensor;
-//   dht.temperature().getSensor(&sensor);
-//   dht.humidity().getSensor(&sensor);
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
 
-//   delayMS = 2000;
-// }
+  delayMS = 2000;
+}
 
-// void loop() {
+void loop() {
 
 
-//   delay(delayMS);
+  delay(delayMS);
 
-//   int photoValue = analogRead(A0);
-//   Serial.print(F("Photoresistor value: "));
-//   Serial.print(photoValue);
-//   Serial.print("  /   ");
+  int photoValue = analogRead(A0);
+  Serial.print(F("Photoresistor value: "));
+  Serial.print(photoValue);
+  Serial.print("  /   ");
 
-//   sensors_event_t event;
-//   dht.temperature().getEvent(&event);
-//   Serial.print(F("Temperatura: "));
-//   Serial.print(event.temperature);
-//   Serial.print(F("ºC  /   Humidity: "));
-//   dht.humidity().getEvent(&event);
-//   Serial.print(event.relative_humidity);
-//   Serial.println(F("%"));
-// }
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  Serial.print(F("Temperatura: "));
+  Serial.print(event.temperature);
+  Serial.print(F("ºC  /   Humidity: "));
+  dht.humidity().getEvent(&event);
+  Serial.print(event.relative_humidity);
+  Serial.println(F("%"));
+}
