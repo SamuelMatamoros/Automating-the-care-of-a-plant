c705bef6d74744655aefe8896e53ff0441601026
Samuel Matamoros <samuelmatamorosalonso@gmail.com>
2024-02-02 23:10:11 +0100

    Sensor de ultrasonido testeado

--- ArduinoUno_test/src/main.cpp
+++ ArduinoUno_test/src/main.cpp
@@ -3,6 +3,13 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include <Stepper.h>
+#include <NewPing.h>
+
+#define trigPin 10
+#define echoPin 13
+#define maxDist 400
+
+NewPing sonar(trigPin, echoPin, maxDist);
 
 DHT_Unified dht(12, DHT11);
 
@@ -11,30 +18,45 @@ uint32_t delayMS;
 void setup() {
   Serial.begin(9600);
 
-  dht.begin();
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  dht.humidity().getSensor(&sensor);
+  // dht.begin();
+  // sensor_t sensor;
+  // dht.temperature().getSensor(&sensor);
+  // dht.humidity().getSensor(&sensor);
 
-  delayMS = 2000;
+  // delayMS = 2000;
 }
 
 void loop() {
 
+  // delay(delayMS);
 
-  delay(delayMS);
+  // int photoValue = analogRead(A0);
+  // Serial.print(F("Photoresistor value: "));
+  // Serial.print(photoValue);
+  // Serial.print("  /   ");
 
-  int photoValue = analogRead(A0);
-  Serial.print(F("Photoresistor value: "));
-  Serial.print(photoValue);
-  Serial.print("  /   ");
+  // sensors_event_t event;
+  // dht.temperature().getEvent(&event);
+  // Serial.print(F("Temperatura: "));
+  // Serial.print(event.temperature);
+  // Serial.print(F("ºC  /   Humidity: "));
+  // dht.humidity().getEvent(&event);
+  // Serial.print(event.relative_humidity);
+  // Serial.println(F("%"));
 
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  Serial.print(F("Temperatura: "));
-  Serial.print(event.temperature);
-  Serial.print(F("ºC  /   Humidity: "));
-  dht.humidity().getEvent(&event);
-  Serial.print(event.relative_humidity);
-  Serial.println(F("%"));
+  float duration = sonar.ping_median(5);
+
+  // float distance = sonar.ping_cm();
+  float distance = (duration / 2) * 0.0343;
+
+  Serial.print("Distance = ");
+  if (distance >= 400 || distance <= 2) {
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(500);
+  }
+  delay(500);
 }
